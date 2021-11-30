e19db6e4dcb3887101403c0dd8ed364d7ab2d8d9
Annchy <92168721+Annchy@users.noreply.github.com>
2021-11-30 10:42:54 +0300

    Homework 26.11.2021 SQL-2 (group 24)

--- SQL/HW-7_SQL-2.sql
+++ SQL/HW-7_SQL-2.sql
@@ -1,111 +1,128 @@
 -------------------------------------------------------------------
+--     Таблица employees
+
+-- 1. Создать таблицу employees
+--    - id serial primary key,
+--    - employee_name varchar(50) not null
+
 create table employees (
 	id serial primary key,
 	employee_name varchar(50) not null
 );
 
-select * from employees;
+-- 2. Наполнить таблицу employees 70 строками:
 
 insert into employees (employee_name)
 values 	('Jennie Johnson'),
-		('Christopher White'),
-		('Keith Richards'),
-		('Stephen Gordon'),
-		('Stanley Cunningham'),
-		('Harry Garza'),
-		('Nicole Black'),
-		('Tina Williams'),
-		('Gina Jones'),
-		('Velma Williams'),
-		('Brenda Reyes'),
-		('John Andrews'),
-		('Daniel Gonzalez'),
-		('Audrey Ward'),
-		('Charles Bryant'),
-		('Raymond Turner'),
-		('Gail Fernandez'),
-		('Maria Hamilton'),
-		('Douglas Powell'),
-		('Francis Taylor'),
-		('Armando Saunders'),
-		('Mildred Morgan'),
-		('Dorothy Lee'),
-		('Larry Brown'),
-		('Willie Johnston'),
-		('Albert Rogers'),
-		('John Jones'),
-		('Daniel Huff'),
-		('Harold Barker'),
-		('Douglas Vargas'),
-		('Alan Rogers'),
-		('Erik Rose'),
-		('Troy Austin'),
-		('John Taylor'),
-		('Joann Vega'),
-		('Florence Sanchez'),
-		('Kathryn Alvarado'),
-		('William Perry'),
-		('Helen Farmer'),
-		('Thomas Smith'),
-		('Gene Bailey'),
-		('Robert Brown'),
-		('George Flores'),
-		('Patricia Thompson'),
-		('Kristin Sanders'),
-		('Judy Thompson'),
-		('Evelyn Smith'),
-		('Joseph Austin'),
-		('Katie Williams'),
-		('Ella Robinson'),
-		('Kenneth Porter'),
-		('Laurie Norton'),
-		('Kimberly Myers'),
-		('Raul Collins'),
-		('Cheryl Cole'),
-		('Jorge Schultz'),
-		('Renee Davis'),
-		('Virginia Morris'),
-		('William Harris'),
-		('Amy Robinson'),
-		('Patricia Anderson'),
-		('Kathy Allen'),
-		('Neil Barnes'),
-		('Pamela Taylor'),
-		('Alex Allen'),
-		('Charlotte Romero'),
-		('Margaret Johnson'),
-		('David Bradley'),
-		('James Simon'),
-		('Wanda Ramirez');
+	('Christopher White'),
+	('Keith Richards'),
+	('Stephen Gordon'),
+	('Stanley Cunningham'),
+	('Harry Garza'),
+	('Nicole Black'),
+	('Tina Williams'),
+	('Gina Jones'),
+	('Velma Williams'),
+	('Brenda Reyes'),
+	('John Andrews'),
+	('Daniel Gonzalez'),
+	('Audrey Ward'),
+	('Charles Bryant'),
+	('Raymond Turner'),
+	('Gail Fernandez'),
+	('Maria Hamilton'),
+	('Douglas Powell'),
+	('Francis Taylor'),
+	('Armando Saunders'),
+	('Mildred Morgan'),
+	('Dorothy Lee'),
+	('Larry Brown'),
+	('Willie Johnston'),
+	('Albert Rogers'),
+	('John Jones'),
+	('Daniel Huff'),
+	('Harold Barker'),
+	('Douglas Vargas'),
+	('Alan Rogers'),
+	('Erik Rose'),
+	('Troy Austin'),
+	('John Taylor'),
+	('Joann Vega'),
+	('Florence Sanchez'),
+	('Kathryn Alvarado'),
+	('William Perry'),
+	('Helen Farmer'),
+	('Thomas Smith'),
+	('Gene Bailey'),
+	('Robert Brown'),
+	('George Flores'),
+	('Patricia Thompson'),
+	('Kristin Sanders'),
+	('Judy Thompson'),
+	('Evelyn Smith'),
+	('Joseph Austin'),
+	('Katie Williams'),
+	('Ella Robinson'),
+	('Kenneth Porter'),
+	('Laurie Norton'),
+	('Kimberly Myers'),
+	('Raul Collins'),
+	('Cheryl Cole'),
+	('Jorge Schultz'),
+	('Renee Davis'),
+	('Virginia Morris'),
+	('William Harris'),
+	('Amy Robinson'),
+	('Patricia Anderson'),
+	('Kathy Allen'),
+	('Neil Barnes'),
+	('Pamela Taylor'),
+	('Alex Allen'),
+	('Charlotte Romero'),
+	('Margaret Johnson'),
+	('David Bradley'),
+	('James Simon'),
+	('Wanda Ramirez');
 	
 -------------------------------------------------------------------
+--     Таблица salary
 
+-- 3. Создать таблицу salary
+--    - id serial primary key,
+--    - monthly_salary int not null
+ 
 create table salary (
 	id serial primary key,
 	monthly_salary int not null
 );
 
-select * from salary;
+-- 4. Наполнить таблицу salary 15 строками:
 
 insert into salary (monthly_salary)
 values 	(1000),
-		(1100),
-		(1200),
-		(1300),
-		(1400),
-		(1500),
-		(1600),
-		(1700),
-		(1800),
-		(1900),
-		(2000),
-		(2100),
-		(2200),
-		(2300),
-		(2400),
-		(2500);
+	(1100),
+	(1200),
+	(1300),
+	(1400),
+	(1500),
+	(1600),
+	(1700),
+	(1800),
+	(1900),
+	(2000),
+	(2100),
+	(2200),
+	(2300),
+	(2400),
+	(2500);
 
 -------------------------------------------------------------------
+--    Таблица employee_salary
+
+-- 5. Создать таблицу employee_salary
+--    - id serial primary key,
+--    - employee_id int not null unique,
+--    - salary_id int not null
 
 create table employee_salary (
 	id serial primary key,
@@ -113,86 +130,100 @@ create table employee_salary (
 	salary_id int not null
 );
 
-select * from employee_salary;
+-- 6. Наполнить таблицу employee_salary 40 строками:
+--    - в 10 строк из 40 вставить несуществующие employee_id
 
 insert into employee_salary (employee_id, salary_id)
 values 	(3, 7),
-		(1, 4),
-		(5, 9),
-		(40, 13),
-		(23, 4),
-		(11, 2),
-		(52, 10),
-		(15, 13),
-		(26, 4),
-		(16, 1),
-		(33, 7),
-		(72, 16),
-		(65, 12),
-		(34, 2),
-		(84, 12),
-		(2, 5),
-		(79, 4),
-		(4, 4),
-		(28, 6),
-		(73, 1),
-		(31, 8),
-		(7, 12),
-		(42, 6),
-		(59, 7),
-		(98, 10),
-		(46, 14),
-		(12, 9),
-		(61, 10),
-		(81, 7),
-		(49, 4),
-		(17, 2),
-		(62, 10),
-		(77, 15),
-		(66, 14),
-		(87, 11),
-		(51, 3),
-		(93, 9),
-		(20, 6),
-		(69, 9),
-		(90, 5);
+	(1, 4),
+	(5, 9),
+	(40, 13),
+	(23, 4),
+	(11, 2),
+	(52, 10),
+	(15, 13),
+	(26, 4),
+	(16, 1),
+	(33, 7),
+	(72, 16),
+	(65, 12),
+	(34, 2),
+	(84, 12),
+	(2, 5),
+	(79, 4),
+	(4, 4),
+	(28, 6),
+	(73, 1),
+	(31, 8),
+	(7, 12),
+	(42, 6),
+	(59, 7),
+	(98, 10),
+	(46, 14),
+	(12, 9),
+	(61, 10),
+	(81, 7),
+	(49, 4),
+	(17, 2),
+	(62, 10),
+	(77, 15),
+	(66, 14),
+	(87, 11),
+	(51, 3),
+	(93, 9),
+	(20, 6),
+	(69, 9),
+	(90, 5);
 
 -------------------------------------------------------------------
-	
+--    Таблица roles
+
+-- 7. Создать таблицу roles
+--    - id serial primary key,
+--    - role_name int not null unique
+
 create table roles (
 	id serial primary key,
 	role_name int not null unique
 );
-		
-select * from roles;
+
+-- 8. Поменять тип столба role_name с int на varchar(30):
 
 alter table roles 
 alter column role_name type varchar(30);
 
+-- 9. Наполнить таблицу roles 20 строками:
+
 insert into roles (role_name)
 values 	('Junior Python developer'),
-		('Middle Python developer'),
-		('Senior Python developer'),
-		('Junior Java developer'),
-		('Middle Java developer'),
-		('Senior Java developer'),
-		('Junior JavaScript developer'),
-		('Middle JavaScript developer'),
-		('Senior JavaScript developer'),
-		('Junior Manual QA engineer'),
-		('Middle Manual QA engineer'),
-		('Senior Manual QA engineer'),
-		('Project Manager'),
-		('Designer'),
-		('HR'),
-		('CEO'),
-		('Sales manager'),
-		('Junior Automation QA engineer'),
-		('Middle Automation QA engineer'),
-		('Senior Automation QA engineer');
+	('Middle Python developer'),
+	('Senior Python developer'),
+	('Junior Java developer'),
+	('Middle Java developer'),
+	('Senior Java developer'),
+	('Junior JavaScript developer'),
+	('Middle JavaScript developer'),
+	('Senior JavaScript developer'),
+	('Junior Manual QA engineer'),
+	('Middle Manual QA engineer'),
+	('Senior Manual QA engineer'),
+	('Project Manager'),
+	('Designer'),
+	('HR'),
+	('CEO'),
+	('Sales manager'),
+	('Junior Automation QA engineer'),
+	('Middle Automation QA engineer'),
+	('Senior Automation QA engineer');
 
 -------------------------------------------------------------------
-	
+--    Таблица roles_employee
+
+-- 10. Создать таблицу roles_employee
+--     - id serial primary key,
+--     - employee_id int not null unique (внешний ключ для таблицы employees, поле id)
+--     - role_id int not null (внешний ключ для таблицы roles, поле id)
+
 create table roles_employee (
 	id serial primary key,
 	employee_id int not null unique,
@@ -203,48 +234,48 @@ create table roles_employee (
 		references roles (id)
 );
 
-select * from roles_employee;
+-- 11. Наполнить таблицу roles_employee 40 строками:
 
 insert into roles_employee (employee_id, role_id)
 values 	(7, 2),
-		(20, 4),
-		(3, 9),
-		(5, 13),
-		(23, 4),
-		(11, 2),
-		(10, 9),
-		(22, 13),
-		(21, 3),
-		(34, 4),
-		(1, 5),
-		(8, 10),
-		(12, 4),
-		(15, 1),
-		(31, 6),
-		(24, 11),
-		(66, 17),
-		(29, 7),
-		(55, 16),
-		(45, 19),
-		(57, 17),
-		(33, 20),
-		(68, 8),
-		(19, 1),
-		(37, 7),
-		(63, 18),
-		(13, 2),
-		(28, 13),
-		(41, 16),
-		(69, 6),
-		(44, 12),
-		(48, 19),
-		(38, 18),
-		(40, 14),
-		(46, 2),
-		(52, 4),
-		(51, 15),
-		(6, 17),
-		(61, 14),
-		(70, 20);
+	(20, 4),
+	(3, 9),
+	(5, 13),
+	(23, 4),
+	(11, 2),
+	(10, 9),
+	(22, 13),
+	(21, 3),
+	(34, 4),
+	(1, 5),
+	(8, 10),
+	(12, 4),
+	(15, 1),
+	(31, 6),
+	(24, 11),
+	(66, 17),
+	(29, 7),
+	(55, 16),
+	(45, 19),
+	(57, 17),
+	(33, 20),
+	(68, 8),
+	(19, 1),
+	(37, 7),
+	(63, 18),
+	(13, 2),
+	(28, 13),
+	(41, 16),
+	(69, 6),
+	(44, 12),
+	(48, 19),
+	(38, 18),
+	(40, 14),
+	(46, 2),
+	(52, 4),
+	(51, 15),
+	(6, 17),
+	(61, 14),
+	(70, 20);
 
 -------------------------------------------------------------------
